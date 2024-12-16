60ce1cb574a84be34aa78136ddf13baffaa32db9
Vladislav Belousov <vladbelousov@yahoo.com>
2024-12-16 21:26:57 +0000

    Move render classes to jade

---
+++ jade/render/render_context/dx9/render_context_dx9.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifdef JADE_D3D9_SUPPORT
+
+#include "jade/render/render_context/render_context.hpp"
+
+#include <d3d9.h>
+
+namespace jade {
+
+  class RenderContextDX9 final : public RenderContext
+  {
+  public:
+    explicit RenderContextDX9(SDL_Window* window);
+    ~RenderContextDX9() override;
+
+    void draw_scene(const std::function<void()>& callback) override;
+    auto get_drawable_size() -> std::pair<int, int> override;
+    void viewport(int x, int y, int w, int h) override;
+    void clear(float r, float g, float b, float a) override;
+
+  private:
+    IDirect3D9* d3d = nullptr;
+    IDirect3DDevice9* d3d_device = nullptr;
+  };
+
+} // namespace jade
+
+#endif
